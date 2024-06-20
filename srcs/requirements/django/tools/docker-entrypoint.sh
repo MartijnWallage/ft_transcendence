696283250ac2a42afda3ae101ec2629235b9e872
jules bernard <110232371+julesrb@users.noreply.github.com>
2024-06-20 12:18:19 +0200

    Merge pull request #7 from MartijnWallage/base_website
    Base website

--- srcs/requirements/django/tools/docker-entrypoint.sh
+++ srcs/requirements/django/tools/docker-entrypoint.sh
@@ -16,26 +16,25 @@ fi
 # Set the working directory
 cd /django-files/
 
-echo "Waiting for database to be ready..."
-retries=5
-# Uses nc (netcat) to check 
-# if the database service on host database is listening on port 5432. 
-# The -z flag tells nc to just scan for the listening daemons, without sending any data.
-while ! nc -z postgresSQL 5432; do
-    sleep 1
-    retries=$((retries - 1))
-    # If the retry counter reaches 0, it prints an error
-    if [ $retries -le 0 ]; then
-        echo "Database is not available, exiting..."
-        exit 1
-    fi
-done
-echo "Database is ready!"
+    echo "Waiting for database to be ready..."
+    retries=5
+    # Uses nc (netcat) to check 
+    # if the database service on host database is listening on port 5432. 
+    # The -z flag tells nc to just scan for the listening daemons, without sending any data.
+    while ! nc -z postgresSQL 5432; do
+        sleep 1
+        retries=$((retries - 1))
+        # If the retry counter reaches 0, it prints an error
+        if [ $retries -le 0 ]; then
+            echo "Database is not available, exiting..."
+            exit 1
+        fi
+    done
+    echo "Database is ready!"
 
 # python3 manage.py migrate
 python3 manage.py migrate
-#python3 manage.py runserver 0.0.0.0:8443
-daphne -b 0.0.0.0 -e ssl:8443:privateKey=/etc/daphne/ssl/daphne.key:certKey=/etc/daphne/ssl/daphne.crt pong.asgi:application
+python3 manage.py runserver 0.0.0.0:8000
 
 
 # # Generate openssl certificate
@@ -121,4 +120,5 @@ daphne -b 0.0.0.0 -e ssl:8443:privateKey=/etc/daphne/ssl/daphne.key:certKey=/etc
 # # Starts the daphne server, a server specifically for handling Django Channels applications.
 # # config.asgi:application: Specifies the ASGI application to run, defined in the config module.
 # # exec daphne -b 0.0.0.0 -e ssl:8443:privateKey=/etc/daphne/ssl/daphne.key:certKey=/etc/daphne/ssl/daphne.crt config.asgi:application
-# 
+# daphne -b 0.0.0.0 -e ssl:8443:privateKey=/etc/daphne/ssl/daphne.key:certKey=/etc/daphne/ssl/daphne.crt myproject.asgi:application
+
