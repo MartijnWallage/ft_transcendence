9a70ea2478fe6a3bd71701f0fa9b3290fd7420f6
Hongbae Kim <hongbaki@c4c3c4.42berlin.de>
2024-06-24 18:44:40 +0200

    http://127.0.0.1:8080/playpong/hello/ and http://127.0.0.1:8000/playpong/hello/ work

--- srcs/requirements/django/src/pong/settings.py
+++ srcs/requirements/django/src/pong/settings.py
@@ -126,8 +126,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = '/static/'
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 # WhiteNoise configuration
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
