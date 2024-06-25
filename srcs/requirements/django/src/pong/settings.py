7d5aac69613ccfc8fc4218ed2a90c245f83fa3b1
Hongbae Kim <hongbaki@c4b8c3.42berlin.de>
2024-06-25 11:01:29 +0200

    https works

--- srcs/requirements/django/src/pong/settings.py
+++ srcs/requirements/django/src/pong/settings.py
@@ -142,3 +142,4 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
