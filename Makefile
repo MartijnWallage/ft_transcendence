06f3446769e4118c39010627f5bfaca930fe510c
Jules <jbjulesbernard3@gmail.com>
2024-07-01 18:26:19 +0200

    new folder structure

--- Makefile
+++ Makefile
@@ -34,7 +34,7 @@ clean:
 	-docker ps -qa | xargs docker stop
 	-docker ps -qa | xargs docker rm
 	-docker image ls -qa | xargs docker rmi -f
-#	-docker volume ls -q | xargs docker volume rm
+	-docker volume ls -q | xargs docker volume rm
 	-docker network ls -q | xargs
 	-docker network ls -q | xargs docker network rm 2>/dev/null
 
---
+++ new/pong/views.py
@@ -0,0 +1,32 @@
+# from django.shortcuts import render
+# from django.http import HttpResponse
+
+# def play_pong(request):
+#     return render(request, 'Pong.html')
+
+# # Create your views here.
+
+from django.shortcuts import render, redirect
+from .forms import RegisterForm
+from django.contrib.auth import login, logout, authenticate
+
+def play_pong(request):
+    context = {
+        'is_logged_in': request.user.is_authenticated
+    }
+    return render(request, 'main/pong.html', context)
+
+def home(request):
+    return render(request, "main/home.html")
+
+def sign_up(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('/pong')
+    else:
+        form = RegisterForm()
+    
+    return render(request, 'registration/sign_up.html', {"form": form})
