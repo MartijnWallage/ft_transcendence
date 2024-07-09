b2847d45cdc1cb2eca4e1cf66ae578c32b7b9bbc
Jules <jbjulesbernard3@gmail.com>
2024-07-09 17:17:50 +0200

    3d views prepare

--- srcs/containers/django/src/pong/static/js/3dapp.js
+++ srcs/containers/django/src/pong/static/js/3dapp.js
@@ -8,17 +8,17 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.setClearColor(0xc1d1db); // Set background color
 container.appendChild(renderer.domElement);
 
-// Cube 1
-const geometry = new THREE.BoxGeometry(16, 0.5, 12);
-const material = new THREE.MeshBasicMaterial({ color: 0x0a1826 });
-const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
+// field
+const field_geometry = new THREE.BoxGeometry(16, 0.5, 12);
+const field_material = new THREE.MeshBasicMaterial({ color: 0x0a1826 });
+const field = new THREE.Mesh(filed_geometry, filed_material);
+scene.add(field);
 
-// Cube 2
-const geometry2 = new THREE.BoxGeometry(0.3, 0.6, 11.8);
-const material2 = new THREE.MeshBasicMaterial({ color: 0xc1d1db });
-const cube2 = new THREE.Mesh(geometry2, material2);
-scene.add(cube2);
+// Nett
+const nett_geometry = new THREE.BoxGeometry(0.3, 0.6, 11.8);
+const nett_material = new THREE.MeshBasicMaterial({ color: 0xc1d1db });
+const nett_cube = new THREE.Mesh(nett_geometry, nett_material);
+scene.add(nett);
 
 camera.position.z = 10;
 
@@ -33,13 +33,13 @@ function animate() {
 
 animate();
 
-// // Sky
-// const sky = new Sky();
-// sky.scale.setScalar(450000);
-// scene.add(sky);
+    // // Sky
+    // const sky = new Sky();
+    // sky.scale.setScalar(450000);
+    // scene.add(sky);
 
-// const phi = MathUtils.degToRad(90);
-// const theta = MathUtils.degToRad(180);
-// const sunPosition = new THREE.Vector3().setFromSphericalCoords(1, phi, theta);
-// sky.material.uniforms.sunPosition.value.copy(sunPosition);
+    // const phi = MathUtils.degToRad(90);
+    // const theta = MathUtils.degToRad(180);
+    // const sunPosition = new THREE.Vector3().setFromSphericalCoords(1, phi, theta);
+    // sky.material.uniforms.sunPosition.value.copy(sunPosition);
 
