ab11e91b06c0cd021b9c7546117d9a19a74b1329
julesrb <jbjulesbernard3@gmail.com>
2024-08-21 13:29:44 +0200

    remote work

--- srcs/containers/django/tools/docker-entrypoint.sh
+++ srcs/containers/django/tools/docker-entrypoint.sh
@@ -33,5 +33,5 @@ sudo python3 manage.py makemigrations pong
 python3 manage.py makemigrations
 python3 manage.py migrate
 
-uvicorn transcendence.asgi:application --host 0.0.0.0 --port 8000
+gunicorn transcendence.asgi:application --host 0.0.0.0 --port 8000
 # python3 manage.py runserver 0.0.0.0:8000
