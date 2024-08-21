b4fa098e88df65589681a776da6f2c249b927992
Jules <jbjulesbernard3@gmail.com>
2024-08-21 18:34:51 +0200

    remote wip

--- srcs/containers/django/src/pong/consumers.py
+++ srcs/containers/django/src/pong/consumers.py
@@ -1,9 +1,11 @@
 import json
+from collections import deque
 from channels.generic.websocket import AsyncWebsocketConsumer
 
 class PongConsumer(AsyncWebsocketConsumer):
 
-    player_counter = 'A'
+    players = deque()  # Deque to store players' information
+    player_role = 'A'
     
     async def connect(self):
         # Assign the room group name to share state between two players
@@ -18,6 +20,12 @@ class PongConsumer(AsyncWebsocketConsumer):
         await self.accept()
 
     async def disconnect(self, close_code):
+        # Remove player from the deque and broadcast updated list
+        self.players = deque(player for player in self.players if player['channel_name'] != self.channel_name)
+
+        # Broadcast the updated player list to everyone
+        await self.broadcast_player_list()
+
         # Leave room group
         await self.channel_layer.group_discard(
             self.room_group_name,
@@ -30,28 +38,40 @@ class PongConsumer(AsyncWebsocketConsumer):
         message_type = data.get('type')
 
         if message_type == 'connected':
-            # Broadcast the connected status
-            await self.channel_layer.group_send(
-                self.room_group_name,
-                {
-                    'type': 'player_connected',
-                    'player': data.get('player'),
-                    'player_role': self.player_counter,
-                }
-            )
-            # Alternate player role
-            self.player_counter = 'B' if self.player_counter == 'A' else 'A'
+            # Add player to the deque
+            player_info = {
+                'player': data.get('player'),
+                'player_role': self.player_role,
+                'ready': False,
+                'channel_name': self.channel_name
+            }
+            self.players.append(player_info)
+
+            # Alternate player role for the next connection
+            self.player_role = 'B' if self.player_role == 'A' else 'A'
+
+            # Broadcast the updated player list to everyone
+            await self.broadcast_player_list()
 
         elif message_type == 'ready':
-            # Broadcast the ready status
-            await self.channel_layer.group_send(
-                self.room_group_name,
-                {
-                    'type': 'player_ready',
-                    'player': data.get('player'),
-                }
-            )
-            
+            # Set player ready status to True
+            for player in self.players:
+                if player['channel_name'] == self.channel_name:
+                    player['ready'] = True
+                    break
+
+            # Check if all players are ready
+            if all(player['ready'] for player in self.players):
+                await self.channel_layer.group_send(
+                    self.room_group_name,
+                    {
+                        'type': 'game_start',
+                    }
+                )
+
+            # Broadcast the updated player list to everyone
+            await self.broadcast_player_list()
+
         elif message_type == 'game_update':
             # Broadcast the game state to the group
             await self.channel_layer.group_send(
@@ -82,4 +102,31 @@ class PongConsumer(AsyncWebsocketConsumer):
         await self.send(text_data=json.dumps({
             'type': 'game_state',
             'state': event['state']
+        }))
+
+    async def game_start(self, event):
+        # Send the game start signal to WebSocket
+        await self.send(text_data=json.dumps({
+            'type': 'start',
+        }))
+
+    async def broadcast_player_list(self):
+        # Broadcast the entire player list to everyone in the group
+        player_list = [
+            {'player': player['player'], 'player_role': player['player_role'], 'ready': player['ready']}
+            for player in self.players
+        ]
+        await self.channel_layer.group_send(
+            self.room_group_name,
+            {
+                'type': 'player_list',
+                'players': player_list
+            }
+        )
+
+    async def player_list(self, event):
+        # Send the player list to WebSocket
+        await self.send(text_data=json.dumps({
+            'type': 'player_list',
+            'players': event['players']
         }))
