a768447cbdfe9f5c153c0f9c6c47b8602629870d
Jules <jbjulesbernard3@gmail.com>
2024-08-21 16:37:23 +0200

    remote wip

--- srcs/containers/django/src/pong/consumers.py
+++ srcs/containers/django/src/pong/consumers.py
@@ -4,9 +4,9 @@ from channels.generic.websocket import AsyncWebsocketConsumer
 class PongConsumer(AsyncWebsocketConsumer):
 
     player_counter = 'A'
+    player_data = {}  # Dictionary to track player information
     
     async def connect(self):
-        # Assign the room group name to share state between two players
         self.room_group_name = 'pong_game'
 
         # Join room group
@@ -15,7 +15,11 @@ class PongConsumer(AsyncWebsocketConsumer):
             self.channel_name
         )
 
+        # Accept the WebSocket connection
         await self.accept()
+
+        # Send existing player information to the new player
+        await self.send_existing_players()
 
     async def disconnect(self, close_code):
         # Leave room group
@@ -24,24 +28,47 @@ class PongConsumer(AsyncWebsocketConsumer):
             self.channel_name
         )
 
+        # Remove player from the player data
+        player_to_remove = None
+        for player, channel in self.player_data.items():
+            if channel == self.channel_name:
+                player_to_remove = player
+                break
+
+        if player_to_remove:
+            del self.player_data[player_to_remove]
+
+            # Notify other players about the disconnection
+            await self.channel_layer.group_send(
+                self.room_group_name,
+                {
+                    'type': 'player_disconnected',
+                    'player': player_to_remove,
+                }
+            )
+
     async def receive(self, text_data):
-        # Receive message from WebSocket
         data = json.loads(text_data)
         message_type = data.get('type')
 
         if message_type == 'connected':
-            # Broadcast the connected status
+            # Assign player and send player information to the group
+            player_role = self.player_counter
+            self.player_counter = 'B' if self.player_counter == 'A' else 'A'
+            
+            player_name = data.get('player')
+            self.player_data[player_name] = self.channel_name
+
+            # Broadcast the connected player
             await self.channel_layer.group_send(
                 self.room_group_name,
                 {
                     'type': 'player_connected',
-                    'player': data.get('player'),
-                    'player_role': self.player_counter,
+                    'player': player_name,
+                    'player_role': player_role,
                 }
             )
-            # Alternate player role
-            self.player_counter = 'B' if self.player_counter == 'A' else 'A'
-
+        
         elif message_type == 'ready':
             # Broadcast the ready status
             await self.channel_layer.group_send(
@@ -62,8 +89,16 @@ class PongConsumer(AsyncWebsocketConsumer):
                 }
             )
 
+    async def send_existing_players(self):
+        for player, channel in self.player_data.items():
+            # Send each existing player to the new player
+            await self.send(text_data=json.dumps({
+                'type': 'player_connected',
+                'player': player,
+                'player_role': 'A' if self.player_data[player] == 'A' else 'B'
+            }))
+
     async def player_connected(self, event):
-        # Send the connected status to WebSocket
         await self.send(text_data=json.dumps({
             'type': 'player_connected',
             'player': event['player'],
@@ -71,14 +106,18 @@ class PongConsumer(AsyncWebsocketConsumer):
         }))
 
     async def player_ready(self, event):
-        # Send the ready status to WebSocket
         await self.send(text_data=json.dumps({
             'type': 'player_ready',
             'player': event['player']
         }))
 
+    async def player_disconnected(self, event):
+        await self.send(text_data=json.dumps({
+            'type': 'player_disconnected',
+            'player': event['player']
+        }))
+
     async def game_state(self, event):
-        # Send the game state to WebSocket
         await self.send(text_data=json.dumps({
             'type': 'game_state',
             'state': event['state']
--- srcs/containers/django/src/pong/static/js/classes/Game.js
+++ srcs/containers/django/src/pong/static/js/classes/Game.js
@@ -120,7 +120,7 @@ class Game {
 				if (data.type === 'player_connected') {
 					if (data.player === this.loggedUser) {
 						player1.online_role = data.player_role;
-						console.log('local role assigned to" + data.player_role');
+						console.log('local role assigned to' + data.player_role);
 					} else if (data.player !== this.loggedUser) {
 						player2 = new Player(data.player);
 						console.log('player 2 created');
