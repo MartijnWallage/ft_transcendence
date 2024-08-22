069b36ef9fbb623027446a8ab1a0477a9ab94aa8
julesrb <jbjulesbernard3@gmail.com>
2024-08-22 03:12:16 +0200

    remote wip

--- srcs/containers/django/src/pong/static/js/classes/Camera.js
+++ srcs/containers/django/src/pong/static/js/classes/Camera.js
@@ -75,10 +75,11 @@ class Camera {
 		const width = window.innerWidth;
 		const height = window.innerHeight;
 		const x = position === 0 ? -14 : 14;
+		const dx = position === 0 ? -12 : 12;
 		this.camera.position.set(x, 14, 0);
 		if (width < height){
 			const objectWidth = 13;
-			const cameraDistance = this.camera.position.distanceTo(new THREE.Vector3(-12, 0, 0));
+			const cameraDistance = this.camera.position.distanceTo(new THREE.Vector3(dx, 0, 0));
 			this.calculateFovToFitObject(objectWidth, cameraDistance, width / height);
 		}
 		else {
