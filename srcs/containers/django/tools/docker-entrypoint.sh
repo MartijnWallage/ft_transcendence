1afd8c81cead9b20545376855e7484b1d0aea567
Jules <jbjulesbernard3@gmail.com>
2024-08-26 12:21:21 +0200

    wip

--- srcs/containers/django/tools/docker-entrypoint.sh
+++ srcs/containers/django/tools/docker-entrypoint.sh
@@ -3,45 +3,9 @@ set -e
 
 sudo mkdir -p $APP_HOME/media/ && sudo chown -R myuser:mygroup $APP_HOME/media/ && sudo chmod -R 755 $APP_HOME/media/
 
-echo "creating ssl folder..."
-sudo mkdir -p /tmp/daphne/ssl
-echo "creating ssl keys..."
-sudo openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
-        -keyout /tmp/daphne/ssl/daphne.key \
-        -out /tmp/daphne/ssl/daphne.crt \
-        -subj "/C=DE/L=Berlin/O=42Berlin/CN=nginx" 
-
-sudo chmod 700 /tmp/daphne/ssl
-sudo chmod 600 /tmp/daphne/ssl/daphne.key
-sudo chmod 600 /tmp/daphne/ssl/daphne.crt
-
-
-echo "Waiting for database to be ready..."
-retries=5
-while ! nc -z postgresSQL 5432; do
-    sleep 1
-    retries=$((retries - 1))
-    if [ $retries -le 0 ]; then
-        echo "Database is not available, exiting..."
-        exit 1
-    fi
-done
-
-echo "Database is ready!"
-
 # Always run collectstatic
 echo "Collecting static files..."
 sudo -E python3 manage.py collectstatic --noinput
-
-# if [ "$DJANGO_INITIAL_SETUP" = "true" ]; then
-# 	python3 manage.py makemigrations
-# 	python3 manage.py migrate
-# 	python3 manage.py createsuperuser --noinput --username $DJANGO_SUPERUSER_USERNAME --email $DJANGO_SUPERUSER_EMAIL
-# fi
-
-# sudo python3 manage.py makemigrations pong
-# python3 manage.py makemigrations
-# python3 manage.py migrate
 
 if [ "$DJANGO_INITIAL_SETUP" = "true" ]; then
 	python3 manage.py makemigrations
